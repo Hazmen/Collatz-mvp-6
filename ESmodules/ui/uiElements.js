6b0adb1b464f8d6201f391ba1b049d7435906794
Hazmen <speedcubing234@gmail.com>
2026-06-19 15:24:12 +0300

    forever young, i wanna be.. forever young

--- ESmodules/ui/uiElements.js
+++ ESmodules/ui/uiElements.js
@@ -2,7 +2,34 @@ import { $, $$, $$$ } from '../utils/dom.js';
 
 // THERE SHOULD BE ONLY LINKS TO REAL DOM ELEMENTS!!!
 
-export let mainInputField = $$$('number-input');
+export let mainInputField = $$$('number-input'); // INPUT field
+
+// Run process controls
+export const runButton = $('.proc-run');
+export const resetButton = $('.proc-reset');
+export const skipButton = $('.proc-skip'); 
+
+// sequence list Text version
+export let txtList = $('.sequence-list');
+export function txtListObj_create(parent, index, number) {
+    const txtList_div = document.createElement('div');
+
+    const txtList_obj = 
+    `
+    <div class="txtList-obj">
+        <span class="txtList-span">${index}</span>
+        <span class="txtList-num font-bold">${number}</span>
+    </div>
+    `;
+
+    if (parent) {
+        parent.insertAdjacentHTML('beforeend', txtList_obj);
+    }
+}
+
+// idk i need style values for smth
+export let seqListObj_Font = $('.txtList-obj_font');
+
 
 export function preventLetters(inp) {
     inp.addEventListener('beforeinput', (e) => {
